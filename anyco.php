299bddd6a2dd5f6e5d24e21e20fdac29cd2e2844
mrnxc <jasper.jimenez96@yahoo.com.ph>
2017-02-08 12:38:57 +0800

    Update anyco.php

--- anyco.php
+++ anyco.php
@@ -1,7 +1,25 @@
 <?php
-<?php
   require('anyco-ui.inc'); 
+  
+  $conn = oci_connect('hr','hr','//localhost/XE');
   ui_print_header('Department');
+  do_query($conn, 'SELECT * FROM DEPARTMENTS')
   ui_print_footer(date('Y-m-d H:i:s'));
-
+  
+  functions do_query($conn, $query){
+    $stid = oci_parse($conn, $query);
+    $r = oci_execute($stid, OCI_DEFAULT);
+  
+  
+  print ' <table bord3er = "1">';
+   while($row =oci_fetch_array($stid, OCI_RETURN_NULLS)) {
+      print '<tr>'.
+        ($item ?htmlentities($itme): '&nbsp;'). '</td>';
+        
+   }
+   print '</tr>';
+   
+}
+  print '</table>';
+}
   ?>
